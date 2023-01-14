30dd0945c39d8e0dcc7ab9d9e78c5d23b44e2679
woooo <99981511+xhae000@users.noreply.github.com>
2023-01-14 22:17:06 +0900

    403 config

--- src/main/java/com/xhadl/yournotion/ErrorHandler/CustomAuthenticationEntryPoint.java
+++ src/main/java/com/xhadl/yournotion/ErrorHandler/CustomAuthenticationEntryPoint.java
@@ -1,5 +1,6 @@
 package com.xhadl.yournotion.ErrorHandler;
 
+import org.junit.jupiter.api.DisplayName;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
 
@@ -8,6 +9,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
+@DisplayName("401")
 public class CustomAuthenticationEntryPoint implements AuthenticationEntryPoint {
 
     // 로그인 페이지로 넘기기 (401 : unauthorized)
