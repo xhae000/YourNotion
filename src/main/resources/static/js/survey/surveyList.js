4423228273aa6874ab2ec39ed3b9fcfd1e983bb9
woooo <99981511+xhae000@users.noreply.github.com>
2023-01-15 23:43:37 +0900

    add surveyList

---
+++ src/main/resources/static/js/survey/surveyList.js
@@ -0,0 +1,37 @@
+$(document).ready(function(){
+
+            $('.survey-btn').each(function(){
+
+                  if($(this).data("is-in-session") == false){
+                        $(this)
+                            .css('background-color','#adadad')
+                            .css('cursor','default')
+                            .text("참여 기간이 지난 설문입니다");
+
+                        return;
+                  }
+
+                  const target_gender = $(this).data('gender');
+                  const target_startAge = Number($(this).data('start-age'));
+                  const target_endAge = Number($(this).data('end-age'));
+
+                  if(gender == null) // 비회원이면 null 들어옴 -> default 설정
+                        return;
+
+                  let isAvailable = true;
+
+                  if(target_gender != "irr" && target_gender != gender)
+                        isAvailable = false;
+                  else if (age < target_startAge || age > target_endAge)
+                        isAvailable = false;
+
+
+                  if (!isAvailable) { // 참여할 수 없는 설문일 때 (성별조건, 나이조건에 의해)
+                        $(this)
+                            .css('background-color','#8BB7FF')
+                            .css('cursor','default')
+                            .text("설문 대상이 아닙니다");
+                  }
+            });
+
+});
