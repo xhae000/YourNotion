a3a06320b5436ee67427380288b4b82149efdb77
woooo <99981511+xhae000@users.noreply.github.com>
2023-01-15 23:42:29 +0900

    set format

--- src/main/java/com/xhadl/yournotion/DTO/SurveyDTO.java
+++ src/main/java/com/xhadl/yournotion/DTO/SurveyDTO.java
@@ -1,13 +1,14 @@
 package com.xhadl.yournotion.DTO;
 
+import com.xhadl.yournotion.Formatter.SurveyListFormatter;
 import lombok.*;
 
 @Getter
 @Setter
 @ToString
 @NoArgsConstructor
-@AllArgsConstructor
 public class SurveyDTO {
+    int id;
     String title;
     String des;
     String gender;
@@ -15,5 +16,26 @@ public class SurveyDTO {
     int start_age;
     int end_age;
     String time;
+    String category;
+
+    /* additional variable */
+    String participants;
+    String isInSession;
+
+
+    /* format variable */
+    String format_age;
+    String format_time;
+    String format_gender;
+
+
+    public void formatForSurveyList(SurveyListFormatter formatter, int count){
+        this.format_time = formatter.formatSession(this.time).get("formatSession");
+        this.isInSession = formatter.formatSession(this.time).get("isInSession");
+
+        this.format_gender = formatter.formatGender(this.gender);
+        this.format_age =  formatter.formatAge(this.start_age, this.end_age);
+        this.participants = formatter.formatParticipants(count);
+    }
 
 }
