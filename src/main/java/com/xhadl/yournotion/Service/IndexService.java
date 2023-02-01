d60ec6f43d63f6e9f31f599e4c3de3dca44c5d50
woooo <99981511+xhae000@users.noreply.github.com>
2023-02-01 19:18:36 +0900

    Index Service 추가

---
+++ src/main/java/com/xhadl/yournotion/Service/IndexService.java
@@ -0,0 +1,7 @@
+package com.xhadl.yournotion.Service;
+
+import org.springframework.ui.Model;
+
+public interface IndexService {
+    public String loadIndex(Model model);
+}
---
+++ src/main/java/com/xhadl/yournotion/ServiceImpl/IndexServiceImpl.java
@@ -0,0 +1,20 @@
+package com.xhadl.yournotion.ServiceImpl;
+
+import com.xhadl.yournotion.Repository.SurveyRepository;
+import com.xhadl.yournotion.Service.IndexService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.ui.Model;
+
+@Service
+public class IndexServiceImpl implements IndexService {
+
+    @Autowired
+    SurveyRepository surveyRepository;
+    @Override
+    public String loadIndex(Model model) {
+        model.addAttribute("surveyCount", surveyRepository.countBy());
+        return "/index";
+    }
+
+}
