1a1c449e226522fd5419aed6b46b2a15904f4dd4
woooo <99981511+xhae000@users.noreply.github.com>
2023-02-01 18:59:50 +0900

    질문, 질문수 select 추가

--- src/main/java/com/xhadl/yournotion/Repository/QuestionRepository.java
+++ src/main/java/com/xhadl/yournotion/Repository/QuestionRepository.java
@@ -4,8 +4,14 @@ import com.xhadl.yournotion.Entity.QuestionEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface QuestionRepository  extends JpaRepository<QuestionEntity, Long> {
 
     public QuestionEntity save(QuestionEntity questionEntity);
+
+    public List<QuestionEntity> findAllBySurveyId(int surveyId);
+
+    public int countBySurveyId(int id);
 }
