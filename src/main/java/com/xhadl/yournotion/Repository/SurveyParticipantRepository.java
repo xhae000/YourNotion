243ef37de96cde79178fded0e153e38d75023066
woooo <99981511+xhae000@users.noreply.github.com>
2023-02-02 20:25:08 +0900

    설문 참여 완료 로직 추가

--- src/main/java/com/xhadl/yournotion/Repository/SurveyParticipantRepository.java
+++ src/main/java/com/xhadl/yournotion/Repository/SurveyParticipantRepository.java
@@ -1,10 +1,13 @@
 package com.xhadl.yournotion.Repository;
 
+import com.xhadl.yournotion.Entity.SurveyAvailableEntity;
 import com.xhadl.yournotion.Entity.SurveyParticipantEntity;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface SurveyParticipantRepository extends JpaRepository<SurveyParticipantEntity, Long> {
+    public SurveyParticipantEntity save(SurveyAvailableEntity surveyAvailableEntity);
     public int countBySurveyId(int survey_id);
+    public SurveyParticipantEntity findBySurveyIdAndParticipantId(int surveyId, int participantId);
 }
