13871672f691eee03afca8c09899fbfd496ae3b0
woooo <99981511+xhae000@users.noreply.github.com>
2023-02-04 15:52:29 +0900

    헤더 메뉴 생성

--- src/main/resources/static/js/common/header.js
+++ src/main/resources/static/js/common/header.js
@@ -3,5 +3,7 @@ $(document).ready(function(){
     $('.login-btn').click(()=>location.href="/login");
     $('.join-btn').click(()=>location.href="/join");
     $('.bar-title').click(()=>location.href="/");
+    $('.my-survey').click(()=>location.href="/survey/mySurvey");
+    $('.my-want').click(()=>location.href="/survey/myWant");
 
 });
