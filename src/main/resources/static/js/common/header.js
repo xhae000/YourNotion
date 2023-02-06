3ec0232c4eba867bd6d22a8dd6ee52237f5a2b6b
woooo <99981511+xhae000@users.noreply.github.com>
2023-02-06 17:18:20 +0900

    메뉴 수정

--- src/main/resources/static/js/common/header.js
+++ src/main/resources/static/js/common/header.js
@@ -1,9 +1,10 @@
 $(document).ready(function(){
+    $('.bar-title').click(()=>location.href="/");
+
+    $('.participated-survey').click(()=>location.href="/survey/participatedSurvey");
+    $('.wanted-survey').click(()=>location.href="/survey/wantedSurvey");
+    $('.made-survey').click(()=>location.href = "/survey/madeSurvey");
 
     $('.login-btn').click(()=>location.href="/login");
     $('.join-btn').click(()=>location.href="/join");
-    $('.bar-title').click(()=>location.href="/");
-    $('.my-survey').click(()=>location.href="/survey/mySurvey");
-    $('.my-want').click(()=>location.href="/survey/myWant");
-
 });
